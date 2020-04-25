569a9c04f0d69d3bda9d1429e1a137079f8b8b23
Arjun Gandhi <arjungandhi06@gmail.com>
2020-04-25 18:59:53 -0400

    is done

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+site_url="https://happybirthdayyouoldassboi.arjungandhi.com/images/"
+
+stage_data={
+  "1": {
+        "big_prompt":"Yo Fucker Let's play A Shitty Game",
+        "little_prompt":"Corona is a single chinese monkey with a shotgun that got shrunk down escaped.",
+        "rules":"Listen Up  Bitch. This isn't Hard But I'm sure we'll fuck it up.\n After each round, you get a hint to guess the password. We keep playing until you get it right.",
+        "password":"OMACWARH"
+        },
+  "2": {
+        "big_prompt":"Lets go make Cooper feel bad cause he can't draw!",
+        "little_prompt":"Fun fact, I can punt a baby 30 yards. Don't ask how I know that.",
+        "rules":"These rules haven't changed I can't be fucked to type all that again.",
+        "password":"rubber ducky"
+        },
+  "3": {
+        "big_prompt":"Time To Bend Over and Get Lashed ;)",
+        "little_prompt":"Epstien didn't kill himself.",
+        "rules":"OOOOOHHH baby a rule change. See Now. You have to be good.\n You gotta get atleast a bronze medal to get a hint.",
+        "password":"Wes Anderson"
+        },
+  "4": {
+        "big_prompt":"Tie a Noose and Jump off a Cliff",
+        "little_prompt":"beep booop. I'm a mother fucking dinosaur. beep beep bitch.",
+        "rules":"If you can't figure this one out disown yourself.",
+        "password":"Atwater Kent"
+       },
+  "5": {
+        "big_prompt":"Uh Shit Fuck. You actually a made it this far",
+        "little_prompt":"Here's a prize I guess",
+        "rules":"REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",
+        "password":"Theres no fucking password dont type shit you fuck."
+  },
+  }
+
+
+wrong=[ "This is papa penguin speaking, and I'm dissapointed",
+        "Do better or I'll outsource your job to my country",
+        "Come on collin, I didn't want you to se this",
+        "Your mother regrets not swallowing to this day",
+        "beep beep you suck.",
+        "We're gonna be here all week arent we.",
+        "The only pokemon you deserve is bidoof.",
+        "A slowpoke would easily outpace you.",
+        "F",
+        "This is why your name has to L's"
+      ]
+
+pholder=[
+      "10 Digit SSN Goes Here.....",
+      "Whats Your Mothers Maiden Name....",
+      "Name of the Town you were born in.....",
+      "Bank account Password Pls.....",
+      "Date of Birth....",
+      "What Is your favorite book....",
+      "Where did you go to high school......",
+      "What was the name of your first pet......."
+]
+
 document.addEventListener('DOMContentLoaded', (event) => {
   load()
 })
@@ -9,53 +68,77 @@ async function searchFunction(e) {
   var inputVal = document.getElementById("query").value
   //show button is loading
   var form = document.getElementById("loading")
-  form.classList.add("loading")
-  //make api requst
-  var res = await postData('https://covid-api.arjungandhi.com/', {
-    query: inputVal
-  })
-  data = JSON.parse(res.body)
-  console.log(data)
-  //putting data in modal
-  var medbar=document.getElementById("medical")
-  var value=Math.round(data.medical_credibility * 100).toString()
-  medbar.className= "c100 p".concat(value)
-  medbar.firstElementChild.innerHTML=value.concat("%")
-
-  var newsbar=document.getElementById("news")
-  var value=Math.round(data.news_hotness * 100).toString()
-  newsbar.className= "c100 green p".concat(value)
-  newsbar.firstElementChild.innerHTML=value.concat("%")
-
-  var toxbar=document.getElementById("toxicity")
-  var value=Math.round(data.toxicity * 100).toString()
-  toxbar.className= "c100 purple p".concat(value)
-  toxbar.firstElementChild.innerHTML=value.concat("%")
-
-  var negbar=document.getElementById("negative")
-  var value=Math.round(data.negitive_credibility * 100).toString()
-  negbar.className= "c100 red p".concat(value)
-  negbar.firstElementChild.innerHTML=value.concat("%")
-
-
-
-
-
-
-
-  //pop modal up
-  $('.ui.small.modal')
-    .modal('show');
-  //remove loading state
-  form.classList.remove("loading")
+  checkPassword(inputVal)
+  document.getElementById("query").value=""
 }
 
 
-function load() {
-  const form = document.getElementById('covidsearch');
+async function load() {
+  const form = document.getElementById('password');
   form.addEventListener('submit', searchFunction);
+  await updateStage(0)
+  update()
 }
 
+function checkPassword(input){
+  cur_stage=localStorage.getItem('stage')
+  if (input.toLowerCase() == stage_data[cur_stage]["password"].toLowerCase()){
+    updateStage()
+    update()
+
+  } else {
+    var text = wrong[Math.floor(Math.random() * wrong.length)];
+    alert(text)
+  }
+
+}
+
+
+async function updateStage(input){
+  if (localStorage.getItem('stage')==null){
+    localStorage.setItem('stage', '1')
+  }  else if (input != 0 ){
+    cur_stage=localStorage.getItem('stage')
+    localStorage.setItem('stage',(parseInt(cur_stage)+1).toString())
+    }
+}
+
+function update(){
+  update_background()
+  updatePrompt()
+  
+  cur_stage=localStorage.getItem('stage')
+  if(cur_stage=='5'){
+    console.log('hi')
+    doWin()
+  } else{
+    showRules()
+  }
+}
+
+function showRules(){
+  cur_stage=localStorage.getItem('stage')
+  alert(stage_data[cur_stage]['rules'])
+
+}
+
+function update_background(){
+  link='url("'+site_url+localStorage.getItem('stage')+'.jpg")'
+  document.getElementById('content').style.backgroundImage = link;
+}
+
+function doWin(){
+  t=document.getElementById('password').outerHTML='<video controls="controls" width="600" height="400" name="Video Name" src="http://happybirthdayyouoldassboi.arjungandhi.com/fuck.mov"></video>'
+  window.localStorage.removeItem('stage')
+}
+
+
+function updatePrompt(){
+  cur_stage=localStorage.getItem('stage')
+  document.getElementById("big_prompt").innerHTML=stage_data[cur_stage]["big_prompt"]
+  document.getElementById("little_prompt").innerHTML=stage_data[cur_stage]["little_prompt"]
+  document.getElementById("query").placeholder=pholder[Math.floor(Math.random() * pholder.length)]
+}
 
 // Example POST method implementation:
 async function postData(url = '', data = {}) {
